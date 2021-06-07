3f2ce3864295923c6fd732dcbe0f583f0e6ab86d
Odukoya Jesuseyitan <setoryz@gmail.com>
2021-06-07 08:57:05 +0100

    ✨ stars added

--- three-js-demo/main.js
+++ three-js-demo/main.js
@@ -41,6 +41,21 @@ scene.add(lightHelper, gridHelper);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 
+const addStar = () => {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+  const star = new THREE.Mesh(geometry, material);
+
+  const [x, y, z] = [...Array(3)].map(() =>
+    THREE.MathUtils.randFloatSpread(100)
+  );
+
+  star.position.set(x, y, z);
+  scene.add(star);
+};
+
+[...Array(200)].forEach(addStar);
+
 const animate = () => {
   requestAnimationFrame(animate);
 
